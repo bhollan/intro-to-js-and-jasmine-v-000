94ae9afb9c2ee61a834562987f57ea5d48acba2e
Brian Holland <brian.holland@proatria.com>
2016-05-03 16:06:47 -0500

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream (flava){
+  return "I love " + flava;
+};
+
+function shouting (words){
+  return words.toUpperCase();
+};
+
+function roundDown (input) {
+  return Math.floor(input);
+};
+
+function theTruth () {
+  return true
+};
